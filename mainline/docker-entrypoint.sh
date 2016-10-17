b42ddc91826c25b1eb4b2a1df475d162bb0c50ad
Janez Urevc <janez@janezurevc.name>
2016-10-17 10:20:24 +0200

    Revert server name change and wrap replacement token with %.

--- mainline/docker-entrypoint.sh
+++ mainline/docker-entrypoint.sh
@@ -2,11 +2,11 @@
 set -e
 
 # Configure docroot.
-sed -i 's@DOCROOT@'"${DOCROOT}"'@' /etc/nginx/conf.d/*.conf
+sed -i 's@%DOCROOT%@'"${DOCROOT}"'@' /etc/nginx/conf.d/*.conf
 echo "Configured docroot to ${DOCROOT}."
 
 # Set server name.
-sed -i 's/SERVER_NAME/'"${SERVER_NAME}"'/' /etc/nginx/conf.d/*.conf
+sed -i 's/%SERVER_NAME%/'"${SERVER_NAME}"'/' /etc/nginx/conf.d/*.conf
 echo "Configured server name to ${SERVER_NAME}."
 
 exec "$@"
