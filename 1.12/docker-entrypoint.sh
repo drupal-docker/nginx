d3be88926bc7ccef57ddb7d32488794acf0dab00
Jakub Piasecki <jakub@piaseccy.pl>
2018-04-10 12:46:42 +0200

    Allow php service name customizations

--- 1.12/docker-entrypoint.sh
+++ 1.12/docker-entrypoint.sh
@@ -9,4 +9,8 @@ echo "Configured docroot to ${DOCROOT}."
 sed -i 's/%SERVER_NAME%/'"${SERVER_NAME}"'/' /etc/nginx/conf.d/*.conf
 echo "Configured server name to ${SERVER_NAME}."
 
+# Set php service name.
+sed -i 's/%PHP_SERVICE_NAME%/'"${PHP_SERVICE_NAME}"'/' /etc/nginx/conf.d/*.conf
+echo "Configured server name to ${PHP_SERVICE_NAME}."
+
 exec "$@"
